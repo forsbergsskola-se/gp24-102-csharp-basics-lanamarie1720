dfb922b440203f6be56e4ac9633f177f134045e5
lanamarie1720 <lanamarie1720@gmail.com>
2024-09-16 14:15:13 +0200

    P5Variables
    Project upload

---
+++ P5Variables/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string a;
+a = "Hello World";
+Console.WriteLine(a);
+
+string b;
+
+b = a;
+Console.WriteLine(b);
+
+a = "Forsbergs";
+Console.WriteLine(a);
+Console.WriteLine(b);
