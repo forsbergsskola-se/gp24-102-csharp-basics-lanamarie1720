31d78b2365bb8dcd94121a4bb86b230c0928c95b
lanamarie1720 <lanamarie1720@gmail.com>
2024-09-17 09:31:04 +0200

    P6DataTypes
    Project Update

---
+++ P6DataTypes/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string outputString;
+
+outputString = "Hello World";
+
+Console.WriteLine(outputString);
+
+float decimalValue = 0.3f;
+
+Console.WriteLine(decimalValue);
+
+bool isAwesome = true;
+
+Console.WriteLine(isAwesome);
