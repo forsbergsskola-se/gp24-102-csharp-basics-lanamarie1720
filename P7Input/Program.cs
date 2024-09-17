e3ef49f9e58948a121f991af0a4cb7467ed00c13
lanamarie1720 <lanamarie1720@gmail.com>
2024-09-17 10:07:34 +0200

    P7Input
    Project Update

---
+++ P7Input/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What is your name?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine($"Good morning, {name}");
+
